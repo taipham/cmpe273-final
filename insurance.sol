50381523a06041cdbdb858f3fd2706340e43fb1d
randomstyle <providen1990@yahoo.com.vn>
2017-12-04 23:44:35 -0800

    changes

--- insurance.sol
+++ insurance.sol
@@ -1,46 +1,66 @@
 pragma solidity ^0.4.18;
 
 contract InsuranceContract {
+    address insurer;
+
     
-  string fName;
-  uint age;
-  uint value = 1000;
-   
-  mapping (address => uint) pendingWithdrawals;
-
-  function setBuyer(string _fName, uint _age, uint _value) public {
-      fName = _fName;
-      age = _age;
-      value = _value;
-  }
-   
-  function getBuyer() public constant returns (string, uint, uint) {
-      return (fName, age, value);
-  }
+    struct buyer {
+        string name;
+        uint32 age;
+        uint32 baseCost;
+        uint32 totalRebate;
+        uint monthlySendDate;
+        string insureProof;
+    }
   
-  function calculateRebate(uint steps) public returns (uint) {
-      uint rebate = 0;
-      if (steps > 10000) {
-          rebate = 1;
-      }
-      pendingWithdrawals[msg.sender] = rebate;
-      return rebate;
-  }  
+    mapping (address => buyer) buyers;
+    address[] buyerAddresses;
+    
 
-  function getRebateValue() public constant returns (uint) {
-      return pendingWithdrawals[msg.sender];
-  }
+    function InsuranceContract() public {
+        insurer = msg.sender;
+    }
+    
 
-  function () public payable {
- 
-  }
+    function regInsurance(string _name, uint32 _age, uint32 _baseCost) public returns (string) {
+        var aBuyer = buyers[msg.sender];
+        aBuyer.name = _name;
+        aBuyer.age = _age;
+        aBuyer.baseCost = _baseCost;
+        aBuyer.totalRebate = 0;
+        aBuyer.monthlySendDate = now;
+        // randomize a string here => hash the string => store the hash here for now.
+        aBuyer.insureProof = "random string hash";
+        buyerAddresses.push(msg.sender);
+        return aBuyer.insureProof;
+    }
+  
+    function sendMonthly(uint _steps) public payable{
+        calculateRebate(_steps);
+    }
+  
+    function getMyInfo() public view returns (string, uint, uint, uint, uint, string) {
+        var info = buyers[msg.sender];
+        return (info.name, info.age, info.baseCost, info.totalRebate, info.monthlySendDate, info.insureProof);
+    }
+  
+    function calculateRebate(uint steps) private {
+        uint32 rebate = 0;
+        if (steps > 10000) {
+            rebate = 1;
+        }
+        buyers[msg.sender].totalRebate += rebate;
+    }
 
-  function withdraw() {
-      uint amount = pendingWithdrawals[msg.sender];
-      // Remember to zero the pending refund before
-      // sending to prevent re-entrancy attacks
-      pendingWithdrawals[msg.sender] = 0;
-      msg.sender.transfer(amount);
-  }
+//   function getRebateValue() public constant returns (uint) {
+//       return pendingWithdrawals[msg.sender];
+//   }
 
+//   function withdraw() {
+//       uint amount = pendingWithdrawals[msg.sender];
+//       // Remember to zero the pending refund before
+//       // sending to prevent re-entrancy attacks
+//       pendingWithdrawals[msg.sender] = 0;
+//       msg.sender.transfer(amount);
+//   }
 }
